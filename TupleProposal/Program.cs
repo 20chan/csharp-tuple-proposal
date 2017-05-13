cae4b07840d24807b872bd1f15348f55d9487ae3
phillyai <sonagi.philly@gmail.com>
2017-05-13 13:28:05 +0900

    대체 코드 추가

--- TupleProposal/Program.cs
+++ TupleProposal/Program.cs
@@ -11,12 +11,17 @@ namespace TupleProposal
         static void Main(string[] args)
         {
             var a = (1, 2);
-            Console.WriteLine(a.GetType());
+            Console.WriteLine("Type of a : {0}", a.GetType());
+            Console.WriteLine("First value of a : {0}", a.Item1);
 
             var b = (1);
-            Console.WriteLine(b.GetType());
+            Console.WriteLine("Type of b : {0}", b.GetType());
 
-            //var c = (1,); // Parse Error
+
+            //var c = (1,); // Parse Error, Should be =>
+            var c = new ValueTuple<int>(1);
+            Console.WriteLine("Type of c : {0}", c.GetType());
+            Console.WriteLine("First value of c : {0}", c.Item1);
         }
     }
 }
